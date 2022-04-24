dd4e84120d105003d2de8a62344ec270a3c1d700
Qianqian Fang <fangqq@gmail.com>
2022-04-24 12:04:38 -0400

    add python file parsing and benchmarking scripts

---
+++ python/meshtest.py
@@ -0,0 +1,91 @@
+"""meshtest.py - loading speed benchmark for various triangular-mesh file formats
+
+This module test the parsing speed for various commonly used and emerging surface
+mesh file formats. Along with the benchmarks for JavaScript and MATLAB, this script
+provides a meaningful comparison on the trade-offs between file sizes and parsing speed
+offered by using different mesh formats. Additionally, similar to the sample scripts
+for JavaScript and MATLAB, this script provides a simple list of file loading/parsing
+commands to use these formats in Python.
+
+To run this script, one should install the below dependencies via pip
+
+    python3 -mpip install nibabel jdata bjdata numpy-stl meshio numpy
+
+Then one can simply run
+
+    python3 meshtest.py
+
+inside the current folder.
+
+Author: Qianqian Fang <q.fang at neu.edu>
+
+"""
+
+import nibabel as nib           # read .gii
+import jdata as jd              # read .jmsh, .bmsh
+import json                     # read .json
+import mz3                      # read .mz3
+import stl                      # read .stl
+import meshio                   # read .obj
+
+import os
+import time
+
+files=["gz.gii", "gz.mz3", "raw.mz3", "obj.obj", "stl.stl", "zlib.jmsh", "zlib.bmsh", "raw.min.json", "raw.bmsh"] #"lzma.bmsh"
+
+def loadmeshx10(fname):
+    filename=os.getcwd() + '/../meshes/'+fname
+    expectednode=163842
+    expectedface=327680
+
+    # repeating 10 times, and measure the total run-times for the last 9 iterations
+    for x in range(0, 10):
+        if(x==1):
+            t=time.time()
+        points, tris = loadmesh(filename)
+
+    runtime=(time.time()-t)*1000
+    print("{}\t{}".format(fname, runtime))
+
+    if(fname.find(".stl")<0 and len(points)!=expectednode and len(points)!=expectednode*3):
+        print("node size mismatch: expected {}, got {}".format(expectednode,len(points)))
+    if(len(tris)!=expectedface and len(tris)!=expectedface*3):
+        print("face size mismatch: expected {}, got {}".format(expectedface,len(tris)))
+    return runtime
+
+def loadmesh(filename):
+    ext=os.path.splitext(filename)[-1];
+    points=[]
+    tris=[]
+    if(ext=='.gii'):
+        obj = nib.load(filename)
+        tris, points= obj.agg_data()
+    elif(ext == '.mz3'):
+        points, tris = mz3.load_mz3_mesh(filename, False)
+    elif(ext == '.stl'):
+        obj = stl.mesh.Mesh.from_file(filename)
+        points = obj.points
+        tris = obj.vectors
+    elif(ext == '.obj'):  # or .obj, .off, .vtk, .ply, ..., but slow
+        obj = meshio.read(filename)
+        points = obj.points
+        tris = obj.cells[0].data
+    elif(ext == '.jmsh' or ext == '.bmsh'):
+        obj = jd.load(filename)
+        points = obj['MeshVertex3']
+        tris = obj['MeshTri3']
+    elif(ext == '.json'):
+        with open(filename, 'r') as fp:
+            obj = json.load(fp)
+        points = obj['MeshVertex3']
+        tris = obj['MeshTri3']
+    else:
+        print('skipping file'+fname)
+        return points, tris
+    return points, tris
+
+
+# run benchmark
+res=map(loadmeshx10, files)
+runtimes=list(res)
+
---
+++ python/mz3/mz3.py
@@ -0,0 +1,124 @@
+"""mz3 module
+
+This module is reformatted from https://github.com/neurolabusc/surf-ice/blob/master/mz3/mz3.py
+"""
+
+__all__ = ['load_mz3_mesh']
+
+##====================================================================================
+## dependent libraries
+##====================================================================================
+
+import struct
+import gzip
+
+##====================================================================================
+## 
+##====================================================================================
+
+def read(fnm, isVerbose):
+    invalid_mz3 = (None, None, None, None)
+    faces = []
+    verts = []
+    rbga = []
+    scalar = []
+    with open(fnm, 'rb') as f:
+        hdrBytes = 16
+        data = f.read(hdrBytes)
+        # header is 16 bytes LITTLE-ENDIAN
+        #  UINT16 : MAGIC
+        #  UINT16 : ATTR
+        #  UINT32 : NFACE
+        #  UINT32 : NVERT
+        #  UINT32 : NSKIP
+        hdr = struct.unpack_from('<HHIII', data)
+        if hdr[0] != 23117:  # incorrect magic: assume gzip
+            f = gzip.open(fnm, 'rb')
+            data = f.read(hdrBytes)
+            hdr = struct.unpack_from('<HHIII', data)
+            if hdr[0] != 23117:  # incorrect magic: not mz3
+                print("Not a valid MZ3 file")
+                return invalid_mz3
+            if isVerbose:
+                print("GZ Compressed")
+        MAGIC = hdr[0]
+        ATTR = hdr[1]
+        NFACE = hdr[2]
+        NVERT = hdr[3]
+        NSKIP = hdr[4]
+        # read attributes
+        isFACE = (ATTR & 1) != 0
+        isVERT = (ATTR & 2) != 0
+        isRGBA = (ATTR & 4) != 0
+        isSCALAR = (ATTR & 8) != 0
+        # report contents
+        if isVerbose:
+            print("MAGIC %d ATTR %d" % (MAGIC, ATTR))
+            print("NFACE %d NVERT %d NSKIP %d" % (NFACE, NVERT, NSKIP))
+            print(" isFACE %r isVERT %r" % (isFACE, isVERT))
+            print(" isRGBA %r isSCALAR %r" % (isRGBA, isSCALAR))
+        # quit if file does not make sense
+        if ATTR > 15:
+            print("Unable to read future version of MZ3 file")
+            return invalid_mz3
+        if NVERT < 1:
+            print("Unable to read MZ3 files without vertices")
+            return invalid_mz3
+        if (NFACE < 1) & (isFACE):
+            print("MZ3 files with isFACE must specify NFACE")
+            return invalid_mz3
+        # read faces
+        f.seek(hdrBytes+NSKIP)
+        if isFACE:
+            data = f.read(12 * NFACE)  # each face is 3 UINT32 vertex indices
+            str = '<{}I'.format(3*NFACE)
+            faces = struct.unpack_from(str, data)
+        # read vertices
+        f.seek(hdrBytes+NSKIP+(isFACE * NFACE * 3 * 4))
+        if isVERT:
+            data = f.read(12 * NVERT)  # each vertex is 3 FLOAT32 (xyz)
+            str = '<{}f'.format(3*NVERT)
+            verts = struct.unpack_from(str, data)
+        # read RGBA colors
+        f.seek(hdrBytes+NSKIP+(isFACE * NFACE * 12)+(isVERT * NVERT * 12))
+        if isRGBA:
+            data = f.read(4 * NVERT)  # each vertex has UINT32 RGBA value
+            str = '<{}I'.format(NVERT)
+            rbga = struct.unpack_from(str, data)
+        # read Scalar Intensity
+        f.seek(hdrBytes+NSKIP+(isFACE * NFACE * 12)
+               + (isVERT * NVERT * 12)+(isRGBA * NVERT * 4))
+        if isSCALAR:
+            data = f.read(4 * NVERT)  # each vertex has FLOAT32 scalar value
+            str = '<{}f'.format(NVERT)
+            scalar = struct.unpack_from(str, data)
+    # Optional verbose reporting
+    if isVerbose & (len(faces) > 0):
+        NFACE = len(faces) // 3
+        j = 0
+        for i in range(NFACE):
+            print("%d face %d %d %d" % (i, faces[j], faces[j+1], faces[j+2]))
+            j = j + 3
+    if isVerbose & (len(verts) > 0):
+        NVERT = len(verts) // 3
+        j = 0
+        for i in range(NVERT):
+            print("%d vert %g %g %g" % (i, verts[j], verts[j+1], verts[j+2]))
+            j = j + 3
+    if isVerbose & (len(rbga) > 0):
+        for i in range(len(rbga)):
+            rgba = struct.unpack("4B", struct.pack("I", rbga[i]))
+            print("%d rgba %d %d %d %d" % (i, rgba[0],
+                                           rgba[1], rgba[2], rgba[3]))
+    if isVerbose & (len(scalar) > 0):
+        for i in range(len(scalar)):
+            print("%d scalar %g" % (i, scalar[i]))
+    return faces, verts, rbga, scalar
+
+
+def load_mz3_mesh(filepath, isVerbose):
+    faces, verts, rbga, scalar = read(filepath, isVerbose)
+    if verts is None:
+        print('Invalid file')
+        return
+    return verts, faces
